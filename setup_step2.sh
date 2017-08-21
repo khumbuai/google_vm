20cd7d4c6c18a1d3d77b1f3381c78306b0ed95b8
Christof Henkel <henkel.christof@gmail.com>
2017-08-21 12:55:07 +0200

    no message

---
+++ setup_step2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source ~/.bashrc
+conda list
--- setup_vm.sh
+++ setup_vm.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir Downloads
-cd Downloads/
+mkdir ~/Downloads
+cd ~/Downloads/
 curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
 bash Anaconda3-4.4.0-Linux-x86_64.sh
-source ~/.bashrc
-conda list
+rm Anaconda3-4.4.0-Linux-x86_64.sh
+
+
+
 
 
