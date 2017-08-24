de4476e7665ae89eae507c08ab66681c796d4df4
Christof Henkel <henkel.christof@gmail.com>
2017-08-24 10:41:44 +0200

    removed second step

--- setup_vm.sh
+++ setup_vm.sh
@@ -2,9 +2,9 @@
 
 mkdir ~/Downloads
 cd ~/Downloads/
-curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
-bash Anaconda3-4.4.0-Linux-x86_64.sh
-rm Anaconda3-4.4.0-Linux-x86_64.sh
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
 
 exec bash
 pip install wget
