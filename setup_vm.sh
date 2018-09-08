0a90974c2498c2bd58b65c98a8406d50acf4bf46
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-08 14:56:47 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -6,6 +6,8 @@ curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 
+sudo snap install pycharm-community --classic
+
 exec bash
 pip install wget
 
