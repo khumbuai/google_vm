649b6edd504d7a4833d746d1639eb7d094bdf36f
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-08 10:42:25 +0200

    Create install_vnc.sh

---
+++ install_vnc.sh
@@ -0,0 +1,6 @@
+# https://medium.com/google-cloud/graphical-user-interface-gui-for-google-compute-engine-instance-78fccda09e5c
+
+sudo apt-get update
+sudo apt-get install gnome-core -y
+sudo apt-get install vnc4server -y
+sudo apt-get install gnome-panel -y
