3e9d5c21a35fa90ae78d55ef92bf0e9859fc1b18
maxjeblick <maxjeblick@users.noreply.github.com>
2018-09-10 13:07:58 +0200

    Update setup_vm.sh
    Specified conda and python version

--- setup_vm.sh
+++ setup_vm.sh
@@ -2,15 +2,12 @@
 
 mkdir ~/Downloads
 cd ~/Downloads/
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
+curl -O https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh
+bash Miniconda3-4.5.11-Linux-x86_64.sh
+rm Miniconda3-4.5.11-Linux-x86_64.sh
 
-
+conda install python=3.6
 
 exec bash
 sudo snap install pycharm-community --classic
 pip install wget
-
-
-
