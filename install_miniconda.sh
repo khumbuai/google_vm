5286e3ffbfa6a8f094156c263bce58500608f2fb
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-12 09:34:16 +0200

    Update install_miniconda.sh

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir ~/Downloads
-cd ~/Downloads/
 curl -O https://repo.continuum.io/miniconda/Miniconda3-4.5.1-Linux-x86_64.sh
 bash Miniconda3-4.5.1-Linux-x86_64.sh -b
 rm Miniconda3-4.5.1-Linux-x86_64.sh
 export PATH="~/miniconda3/bin:$PATH" >> ~/.bashrc
-cd ..
-cd google_vm
+
