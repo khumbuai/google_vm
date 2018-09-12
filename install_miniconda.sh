b7b276bfa31c5160f4c4295105b12a7fcaadf48c
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-12 08:53:18 +0200

    Update install_miniconda.sh

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -5,5 +5,5 @@ cd ~/Downloads/
 curl -O https://repo.continuum.io/miniconda/Miniconda3-4.5.1-Linux-x86_64.sh
 bash Miniconda3-4.5.1-Linux-x86_64.sh -b
 rm Miniconda3-4.5.1-Linux-x86_64.sh
-
+echo /home/henkel_christof/miniconda3/bin/activate >> ~/.bashrc
 
