0a74be73d83dae95e108295b8a092788f774a017
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-12 08:59:23 +0200

    Update install_tf_gpu.sh

--- install_tf_gpu.sh
+++ install_tf_gpu.sh
@@ -1,2 +1,1 @@
-sudo pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.1-cp36-cp36m-linux_x86_64.whl
-
+pip install tensorflow-gpu==1.4.1
