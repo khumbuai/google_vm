5a2af6000a4328be80845acedf1eb686bdb75e6d
ChristofHenkel <christof.henkel@campus.lmu.de>
2018-09-12 08:35:09 +0200

    Update setup_vm.sh
    made minconda installation silent

--- setup_vm.sh
+++ setup_vm.sh
@@ -3,10 +3,7 @@
 mkdir ~/Downloads
 cd ~/Downloads/
 curl -O https://repo.continuum.io/miniconda/Miniconda3-4.5.1-Linux-x86_64.sh
-bash Miniconda3-4.5.1-Linux-x86_64.sh
+bash Miniconda3-4.5.1-Linux-x86_64.sh -b
 rm Miniconda3-4.5.1-Linux-x86_64.sh
 
 
-exec bash
-sudo snap install pycharm-community --classic
-pip install wget
