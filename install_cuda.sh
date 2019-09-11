e93924f2b874006715edcd1b29ddc1b953ea18a0
ChristofHenkel <christof.henkel@campus.lmu.de>
2019-09-11 11:12:49 +0200

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -8,3 +8,7 @@ sudo dpkg -i cuda-repo-ubuntu1604-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
+
+echo "export PATH=/usr/local/cuda-10.1/bin:/usr/local/cuda-10.1/NsightCompute-2019.1${PATH:+:${PATH}}" >> .bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> .bashrc
+source .bashrc
