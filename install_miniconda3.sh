47faa0601e5f96045bd51889b535be27884e501b
ChristofHenkel <christof.henkel@campus.lmu.de>
2019-09-11 10:39:46 +0200

    Update install_miniconda3.sh

--- install_miniconda3.sh
+++ install_miniconda3.sh
@@ -1,2 +1,3 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ~/Miniconda3-latest-Linux-x86_64.sh -b
+echo "export PATH=/home/ubuntu/miniconda3/bin:$PATH" >> .bashrc
