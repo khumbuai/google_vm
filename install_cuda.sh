1fea9afc76b0edf72dca587c70ce580ad9006e50
ChristofHenkel <christof.henkel@campus.lmu.de>
2019-09-11 11:10:19 +0200

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,3 +1,6 @@
+#https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#verify-kernel-packages
+sudo apt-get install linux-headers-$(uname -r)
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
 sudo mv cuda-ubuntu1604.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1604-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
