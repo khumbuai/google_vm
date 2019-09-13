fa903c46667e5377095c45118fb92216968942cc
ChristofHenkel <christof.henkel@campus.lmu.de>
2019-09-13 19:08:06 +0200

    Update install_cudatoolkit.sh

--- install_cudatoolkit.sh
+++ install_cudatoolkit.sh
@@ -2,3 +2,4 @@ wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installer
 sudo sh cuda_10.1.243_418.87.00_linux.run --silent --toolkit
 echo "export PATH=/usr/local/cuda-10.1/bin:/usr/local/cuda-10.1/NsightCompute-2019.1${PATH:+:${PATH}}" >> .bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> .bashrc
+rm cuda_10.1.243_418.87.00_linux.run
