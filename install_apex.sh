9779c2388bac544493e7d4cc9b085c04c26e7c5f
ChristofHenkel <christof.henkel@campus.lmu.de>
2019-09-13 19:04:54 +0200

    Update install_apex.sh

--- install_apex.sh
+++ install_apex.sh
@@ -1,9 +1,7 @@
-sudo apt install nvidia-cuda-toolkit
 sudo apt update
 sudo apt install build-essential
 
 git clone https://github.com/NVIDIA/apex
 cd apex
-
 
 pip install --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
